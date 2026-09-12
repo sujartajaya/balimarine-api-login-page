56726b20f8b052642b1a777bbc0ceee89333e63e
Sujarta Jaya <sujarta.jaya@outlook.co.id>
2026-09-12 13:05:27 +0800

    Penambahan Tabel baru

--- api/models/models.py
+++ api/models/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime, TIMESTAMP, text
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, BigInteger, String, TIMESTAMP, ForeignKey, text
+from sqlalchemy.orm import relationship
 from api.database.base import Base
+from sqlalchemy.sql import func
 
 #Table Users
 class User(Base):
@@ -17,11 +18,11 @@ class User(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
     updated_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
-#Table Guests
+# Table Guests
 class Guest(Base):
     __tablename__ = 'guests'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True) # Pakai Integer (max 2.1 Miliar)
     name = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     phone = Column(String(25), nullable=True)
@@ -30,3 +31,30 @@ class Guest(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
     updated_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
+    # Relationship ke ClientDevice
+    devices = relationship("ClientDevice", back_populates="guest", cascade="all, delete-orphan")
+
+
+# Table Client Devices
+class ClientDevice(Base):
+    __tablename__ = "client_devices"
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True) # BigInteger sangat tepat di sini
+    
+    # PERBAIKAN: Tipe data harus Integer (sama dengan Guest.id) dan tambahkan ForeignKey
+    guest_id = Column(Integer, ForeignKey("guests.id", ondelete="CASCADE"), nullable=False, index=True)
+    
+    mac_add = Column(String(255), nullable=True)
+    os_client = Column(String(255), nullable=True)
+    browser_client = Column(String(255), nullable=True)
+    device_client = Column(String(255), nullable=True)
+    brand_client = Column(String(255), nullable=True)
+    model_client = Column(String(255), nullable=True)
+    device_type = Column(String(255), nullable=True)
+    
+    # disamakan dengan tabel Guest menggunakan timezone=True
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+
+    # Relationship ke Guest
+    guest = relationship("Guest", back_populates="devices")
